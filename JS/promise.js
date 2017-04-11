c1199e7e62ce5b2ede6541f7a0f1ba3ed5e0f387
aimergenge <583692276@qq.com>
2017-04-11 18:26:38 +0800

    As each promise can only have a  state of Fulfilled or Rejected, the reason should be replaced by the value.

--- JS/promise.js
+++ JS/promise.js
@@ -1,132 +1,132 @@
-const PENDING = 0, FULFILLED = 1, REJECTED = 2;
+const PENDING = 0, FULFILLED = 1, REJECTED = 2
 
 const utils = {
     isFunction: function (x) {
-        return typeof x === 'function';
+        return typeof x === 'function'
     },
     isObject: function (x) {
-        return Object(x) === x;
+        if (x == null) return false
+        return typeof x === 'object' || typeof x === 'function';
     }
 }
 
 const Promise = function (fn) {
-    this.state = PENDING;
-    this.value = null;
-    this.reason = null;
-    this.handlers = [];
+    this.state = PENDING
+    this.value = null
+    this.handlers = []
 
-    doExecute.call(this, fn, this.resolve, this.reject);
+    doExecute.call(this, fn, this.resolve, this.reject)
 }
 
 function doExecute(fn, resolve, reject) {
-    let done = false, promise = this;
+    let done = false, promise = this
     try {
         fn.call(null, function (value) {
-            if (done) return;
-            resolve.call(promise, value);
-            done = true;
+            if (done) return
+            resolve.call(promise, value)
+            done = true
         }, function (reason) {
-            if (done) return;
-            reject.call(promise, reason);
-            done = true;
+            if (done) return
+            reject.call(promise, reason)
+            done = true
         })
     } catch (e) {
-        if (done) return;
-        reject.call(promise, e);
-        done = true;
+        if (done) return
+        reject.call(promise, e)
+        done = true
     }
 }
 
 const promiseProto = {
     then(onFulfilled, onRejected) {
-        const promise = this;
+        const promise = this
         return new Promise(function (resolve, reject) {
             process.nextTick(function () {
                 promise.handle({
                     onFulfilled: function (value) {
-                        resolve(utils.isFunction(onFulfilled) ? onFulfilled(value) : value);
+                        resolve(utils.isFunction(onFulfilled) ? onFulfilled(value) : value)
                     },
                     onRejected: function (reason) {
-                        reject(utils.isFunction(onRejected) ? onRejected(reason) : reason);
+                        reject(utils.isFunction(onRejected) ? onRejected(reason) : reason)
                     }
                 })
             })
         })
     },
-    handle(obj) {
-        let promise = this, state = this.state, value = this.value, reason = this.reason;
+    handle(coupleHandlers) {
+        let promise = this, state = this.state, value = this.value
         switch (state) {
-            case PENDING: promise.handlers.push(obj); break;
-            case FULFILLED: obj.onFulfilled(value); break;
-            case REJECTED: obj.onRejected(reason);
+            case FULFILLED: return coupleHandlers.onFulfilled(value)
+            case REJECTED: return coupleHandlers.onRejected(value)
+            default: return promise.handlers.push(coupleHandlers)
         }
     },
     fulfill(value) {
-        this.state = FULFILLED;
-        this.value = value;
+        this.state = FULFILLED
+        this.value = value
         for (let {onFulfilled} of this.handlers) {
-            onFulfilled(value);
+            onFulfilled(value)
         }
-        this.handlers = null;
+        this.handlers = null
     },
     reject(reason) {
-        this.state = REJECTED;
-        this.reason = reason;
+        this.state = REJECTED
+        this.value = reason
         for (let {onRejected} of this.handlers) {
-            onRejected(reason);
+            onRejected(reason)
         }
-        this.handlers = null;
+        this.handlers = null
     },
     resolve(x) {
-        let promise = this, done = false;
+        let promise = this, done = false
 
         if (x === promise) {
-            throw new TypeError('The promise and value should not refer to the same object.');
+            promise.reject(new TypeError('The promise and value should not refer to the same object.'))
         } else if (x instanceof Promise) {
-            let state = x.state;
+            let state = x.state
             switch(state) {
-                case PENDING: x.then(promise.resolve.bind(promise), promise.reject.bind(promise)); break;
-                case FULFILLED: promise.fulfill(x.value); break;
-                case REJECTED: promise.reject(x.reason); break;
+                case FULFILLED: return promise.fulfill(x.value)
+                case REJECTED: return promise.reject(x.value)
+                default: return x.then(promise.resolve.bind(promise), promise.reject.bind(promise))
             }
         } else if (utils.isObject(x)) {
-            let then;
+            let then
             try {
-                then = x.then;
+                then = x.then
             } catch (e) {
-                promise.reject(e);
+                promise.reject(e)
             }
             if (utils.isFunction(then)) {
                 try {
-                    then.call(x, resolvePromise, rejectPromise);
+                    then.call(x, resolvePromise, rejectPromise)
                 } catch (e) {
                     if (!done) {
-                        promise.reject(e);
+                        promise.reject(e)
                     }
                 }
             } else {
-                promise.fulfill(x);
+                promise.fulfill(x)
             }
         } else {
-            promise.fulfill(x);
+            promise.fulfill(x)
         }
 
         function resolvePromise(y) {
             if (!done) {
-                promise.resolve(y);
-                done = true;
+                promise.resolve(y)
+                done = true
             }
         }
 
         function rejectPromise(r) {
             if (!done) {
-                promise.reject(r);
-                done = true;
+                promise.reject(r)
+                done = true
             }
         }
     }
 }
 
-Object.assign(Promise.prototype, promiseProto);
+Object.assign(Promise.prototype, promiseProto)
 
-module.exports = Promise;
+module.exports = Promise
